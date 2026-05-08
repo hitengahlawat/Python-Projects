fdd371f80038898b937b614bc980de8b83c5a675
Hiten Gahlawat <hitengahlawat654@gmail.com>
2026-05-08 16:50:34 +0530

    loop modified to take only 5 input numbers

--- the_numberguessing_game.py
+++ the_numberguessing_game.py
@@ -1,47 +1,47 @@
+
 import random
 import pyttsx3
-computer= random.randint(1,100)
+
+computer = random.randint(1, 100)
+
 def speak(text):
-    engine=pyttsx3.init()
+    engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
 
-speak("  IN THIS GAME YOU HAVE TO GUESS A NUMBER \n BETWEEN RANGE 1-100 \n \t\t YOU HAVE ONLY 6 GUESSES")
-print("  IN THIS GAME YOU HAVE TO GUESS A NUMBER \n BETWEEN RANGE 1-100 \n \t\t YOU HAVE ONLY 6 GUESSES")
+message = """
+IN THIS GAME YOU HAVE TO GUESS A NUMBER
+BETWEEN RANGE 1-100
+YOU HAVE ONLY 5 GUESSES
+"""
 
+print(message)
+speak(message)
 
+for i in range(1, 6):
 
-user_input = input("GUESS A NUMBER FROM 1 TO 100 :-")
-if user_input.isdigit():
-    n = int(user_input)
-  
-else:
-    print("Please enter a valid number!")
-    speak("Please enter a valid number!")
+    n = int(input(f"Attempt {i}: GUESS A NUMBER FROM 1 TO 100 :- "))
 
+    if n == computer:
+        print(f"YOU GUESSED PERFECTLY! THAT IS {n}")
+        speak(f"YOU GUESSED PERFECTLY! THAT IS {n}")
 
+        with open("gussing_game_result.txt", "a") as f:
+            print(f"YOU GUESSED PERFECTLY IN {i} TRIES", file=f)
 
-for i in range (1,6):
-    if(computer>n):
-        speak("    GO HIGHER! ")
-        print("    GO HIGHER! ")
-        n=int(input("GUESS THE NUMBER AGAIN :-"))
-    if(n==computer):
-        print(F"    YOU GUESS PERFECT THAT IS {n}")
-        speak(F"    YOU GUESS PERFECT THAT IS {n}")
-        with open("gussing_game_result.txt","a") as f: 
-            print(f" YOU GUSSED PERFECT IN {i+1} TRIES" ,file=f)
-                    
         break
-    if(computer<n):
-        print("    GO LOWER!")
-        speak("    GO LOWER!")
-        n=int(input("GUESS THE NUMBER AGAIN :- "))
+
+    elif n < computer:
+        print("GO HIGHER!")
+        speak("GO HIGHER!")
+
+    else:
+        print("GO LOWER!")
+        speak("GO LOWER!")
+
 else:
-    print(f"YOU TRIES ARE OVER !\n THE NUMBER IS  {computer}")
-    speak(f"YOU TRIES ARE OVER !\n THE NUMBER IS  {computer}")
-    with open("gussing_game_result.txt","a") as f :
+    print(f"YOUR TRIES ARE OVER!\nTHE NUMBER WAS {computer}")
+    speak(f"YOUR TRIES ARE OVER! THE NUMBER WAS {computer}")
+
+    with open("gussing_game_result.txt", "a") as f:
         print(f"YOU FAILED TO GUESS THE NUMBER {computer}", file=f)
-
-
-
